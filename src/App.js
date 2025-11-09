5d759ad7fdf2a4c70402c51cb020784095462884
Vikas Panchal <134390453+vikass01@users.noreply.github.com>
2025-11-09 23:19:11 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from './iconpng.png';
 import './App.css';
 
